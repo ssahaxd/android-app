85ed1bdc840654ce0a2939705d64a68ac32aed7d
Sandip Saha <sandipsmoto@gmail.com>
2017-10-19 03:33:57 +0530

    Friend Request Feature (Incomplete)

--- Hey/app/src/main/java/ssaha/hey/ProfileActivity.java
+++ Hey/app/src/main/java/ssaha/hey/ProfileActivity.java
@@ -1,12 +1,20 @@
 package ssaha.hey;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +32,9 @@ public class ProfileActivity extends AppCompatActivity {
     private DatabaseReference mUsersDatabases;
     private DatabaseReference mFriendReqDatabase;
 
+    //Firebase Auth
+    private FirebaseUser mCurrentUser;
+
     private String mCurrent_state;
 
     //Progress Dialog
@@ -37,10 +48,12 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         // user id of the users profile
-        String user_id = getIntent().getStringExtra("user_id");
+        final String user_id = getIntent().getStringExtra("user_id");
 
         mUsersDatabases = FirebaseDatabase.getInstance().getReference().child("User").child(user_id);
         mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_req");
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
 
         mProfileImage = (ImageView) findViewById(R.id.profile_image);
@@ -48,6 +61,8 @@ public class ProfileActivity extends AppCompatActivity {
         mProfileStatus = (TextView)findViewById(R.id.profile_status);
         mProfileFriendsCount = (TextView)findViewById(R.id.profile_friends_count);
         mProfielSendRequestBtn = (Button)findViewById(R.id.profile_request_btn);
+
+        mCurrent_state = "not_friend";
 
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setTitle("Loading User Data");
@@ -76,7 +91,30 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        mPro
+        mProfielSendRequestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrent_state.equals("not_friend")){
+                    mFriendReqDatabase.child(mCurrentUser.getUid()).child(user_id).child("request_type")
+                            .setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                mFriendReqDatabase.child(user_id).child(mCurrentUser.getUid()).child("request_type")
+                                        .setValue("received").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(ProfileActivity.this, "Request Send", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else{
+                                Toast.makeText(ProfileActivity.this, "Failed to send Request", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
 
     }
